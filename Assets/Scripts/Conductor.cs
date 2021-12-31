16140314be8c8739e9ff370c6716b13a9b36c749
Starpelly <braedonwilcox@gmail.com>
2021-12-31 01:56:51 -0500

    DSP to game time improvement, might be buggy, but works well so far

--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 using Starpelly;
-
-// I CANNOT STRESS THIS ENOUGH, SET "Project Settings/Audio/DSP Buffer Size" to "Best latency" or else AudioSource.time WILL NOT update every frame.
 
 namespace RhythmHeavenMania
 {
@@ -48,14 +47,7 @@ namespace RhythmHeavenMania
         //Conductor instance
         public static Conductor instance;
 
-        //Pause times
-        // private int pauseTime = 0;
-
-        public float beatThreshold;
-
-        private float lastTime;
-        private float lastMst_F;
-        private int framesSinceLastSame;
+        private AudioDspTimeKeeper timeKeeper;
 
         void Awake()
         {
@@ -67,11 +59,13 @@ namespace RhythmHeavenMania
             //Load the AudioSource attached to the Conductor GameObject
             musicSource = GetComponent<AudioSource>();
 
+            timeKeeper = GetComponent<AudioDspTimeKeeper>();
+
             //Calculate the number of seconds in each beat
             secPerBeat = 60f / songBpm;
 
             //Record the time when the music starts
-            dspSongTime = (float)musicSource.time;
+            // dspSongTime = (float)musicSource.time;
 
             //Start the music
             // musicSource.Play();
@@ -79,62 +73,22 @@ namespace RhythmHeavenMania
 
         public void Play(float startBeat)
         {
-            musicSource.Play();
-            
+            timeKeeper.Play();
         }
 
         public void Update()
         {
-            // Conductor.instance.musicSource.pitch = Time.timeScale;
-
-            /*if (Input.GetKeyDown(KeyCode.Space))
-            {
-                pauseTime++;
-                if (pauseTime == 1)
-                    musicSource.Pause();
-                else if (pauseTime > 1) { musicSource.UnPause(); pauseTime = 0; }
-            }*/
-
-            float mst = musicSource.timeSamples / (float)musicSource.clip.frequency;
-            float mst_f = mst + 0;
-
-            if (mst == lastTime && musicSource.isPlaying)
-            {
-                framesSinceLastSame++;
-
-                mst_f = mst_f + (Time.deltaTime * framesSinceLastSame) * musicSource.pitch;
-
-                if (mst_f <= lastMst_F)
-                {
-                    // mst_f = lastMst_F;
-                    float b = lastMst_F + (Time.deltaTime) * musicSource.pitch;
-
-                    mst_f = b;
-                    // print(b);
-                    // print(mst_f + "  " + b + "  " + lastMst_F);
-                }
-                else if (mst_f < lastTime)
-                {
-                    Debug.LogError("What the fuck.");
-                }
-
-                // print($"{lastMst_F}, {mst_f}");
-            }
-            else
-            {
-                framesSinceLastSame = 0;
-            }
-
-            lastTime = mst;
-            lastMst_F = mst_f;
+            if (!musicSource.isPlaying) return;
 
             //determine how many seconds since the song started
-            songPosition = (float)(mst_f - dspSongTime - firstBeatOffset);
+            // songPosition = (float)(timeKeeper.dspTime - dspSongTime - firstBeatOffset);
+            songPosition = (float)timeKeeper.GetCurrentTimeInSong();
+            print(songPosition);
 
             //determine how many beats since the song started
             songPositionInBeats = songPosition / secPerBeat;
             // print($"{mst_f}(AudioSource.time), {Time.frameCount}(Time.fasrameCount)");
-            // print($"{musicSource.time}(0), {mst_f}");
+            // print($"{musicSource.time}(0), {songPosition}");
 
 
             //calculate the loop position
@@ -155,69 +109,6 @@ namespace RhythmHeavenMania
         {
             this.songBpm = bpm;
             secPerBeat = 60f / songBpm;
-        }
-
-        public bool InThreshold(float beat)
-        {
-            //Check if the beat sent falls within beatThreshold
-            //Written to handle the looping
-            if (beat <= beatThreshold + 1)
-            {
-                //Debug.Log("Case 1, beat is close to 1");
-                if (loopPositionInBeats > beat + beatThreshold)
-                {
-                    if (loopPositionInBeats >= (beat + songPositionInBeats - 1) + beatThreshold)
-                    {
-                        //Debug.Log("LoopPos just below loop point");
-                        return true;
-                    }
-                    else
-                    {
-                        //Debug.Log("LoopPos not within beat threshold");
-                    }
-                }
-                else
-                {
-                    //Debug.Log("Case 1, loopPos between loop point and beat threshold");
-                }
-            }
-            else if (beat < (songPositionInBeats + 1 - beatThreshold))
-            {
-                //Debug.Log("Case 2, beat is far from loop point.");
-                if (loopPositionInBeats >= beat - beatThreshold && loopPositionInBeats <= beat + beatThreshold)
-                {
-                    //Debug.Log("LoopPos within threshold");
-                    return true;
-                }
-            }
-            else if (beat >= (songPositionInBeats + 1 - beatThreshold))
-            {
-                //Debug.Log("Case 3, beat is close to loop point");
-                if (loopPositionInBeats < beat)
-                {
-                    if (loopPositionInBeats >= beat - beatThreshold)
-                    {
-                        //Debug.Log("LoopPos just below beat");
-                        return true;
-                    }
-                    else if (loopPositionInBeats < (beat - songPositionInBeats + 1) - beatThreshold)
-                    {
-                        //Debug.Log("LoopPos just above loop point");
-                        return true;
-                    }
-                }
-                else
-                {
-                    //Debug.Log("LoopPos just above beat");
-                    return true;
-                }
-
-            }
-            else
-            {
-                Debug.LogError("Strange Case. Where is this beat? This should never happen");
-            }
-            return false;
         }
     }
 }
