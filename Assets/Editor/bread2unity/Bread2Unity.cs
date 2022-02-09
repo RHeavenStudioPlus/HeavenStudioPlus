e1811674803fcf2b4fead864915a0ed0eb9ddb56
Braedon <braedonwilcox@gmail.com>
2022-02-09 01:16:16 -0500

    bread2unity started but not really (read desc)
    This is really just an excuse to try out the Unity Editor class. But I believe this can be useful for actually making this program.
    Do with that information what you will.

---
+++ Assets/Editor/bread2unity/Bread2Unity.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+using Starpelly;
+
+namespace Bread2Unity
+{
+    public class Bread2Unity : EditorWindow
+    {
+        public const string editorFolderName = "bread2unity";
+
+        [MenuItem("Tools/bread2unity")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow<Bread2Unity>("bread2unity");
+        }
+
+        public void OnGUI()
+        {
+            Texture logo = (Texture)AssetDatabase.LoadAssetAtPath($"Assets/Editor/{editorFolderName}/logo.png", typeof(Texture));
+            GUILayout.Box(logo, new GUILayoutOption[] { GUILayout.ExpandWidth(true), GUILayout.Height(60) });
+            GUILayout.Space(30);
+
+            GUIStyle desc = EditorStyles.label;
+            desc.wordWrap = true;
+            desc.fontStyle = FontStyle.BoldAndItalic;
+
+
+            GUILayout.Box("bread2unity is a tool built with the purpose of converting RH Megamix and Fever animations to unity. And to generally speed up development by a lot." +
+                "\nCreated by Starpelly.", desc);
+
+            GUILayout.Space(120);
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Bread Download", GUILayout.Height(40)))
+            {
+                Application.OpenURL("https://github.com/rhmodding/bread");
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
