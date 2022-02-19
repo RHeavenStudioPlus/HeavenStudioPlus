bef5518b8f0bd9e08b12f9445d3d3a402b592815
Braedon <braedonwilcox@gmail.com>
2022-02-19 16:03:45 -0500

    Tap Trial: Player tap animation

--- Assets/Scripts/Games/TapTrial/TapTrial.cs
+++ Assets/Scripts/Games/TapTrial/TapTrial.cs
@@ -2,9 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using RhythmHeavenMania.Util;
+
 namespace RhythmHeavenMania.Games.TapTrial
 {
     public class TapTrial : Minigame
     {
+        [Header("References")]
+        public TapTrialPlayer player;
+
+        public static TapTrial instance { get; set; }
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        public void Tap(float beat)
+        {
+            MultiSound.Play(new MultiSound.Sound[]
+            {
+                new MultiSound.Sound("tapTrial/ook",   beat),
+                new MultiSound.Sound("tapTrial/tap",   beat + 1.0f),
+            });
+
+            GameObject beatAction = new GameObject();
+            beatAction.transform.SetParent(this.transform);
+            BeatAction.New(beatAction, new List<BeatAction.Action>()
+            {
+                new BeatAction.Action(beat + 0.0f, delegate { player.anim.Play("TapPrepare", 0, 0); }),
+                new BeatAction.Action(beat + 1.0f, delegate { player.anim.Play("Tap", 0, 0); }),
+            });
+        }
+
+        public void DoubleTap(float beat)
+        {
+
+        }
+
+        public void TripleTap(float beat)
+        {
+
+        }
+
+        public void JumpTap(float beat)
+        {
+
+        }
+
+        public void FinalJumpTap(float beat)
+        {
+
+        }
     }
 }
