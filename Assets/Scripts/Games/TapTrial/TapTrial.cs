00ba7dd3b76c9d26bd1d89f4f1f503f0bb8a75c5
Braedon <braedonwilcox@gmail.com>
2022-02-19 13:51:46 -0500

    Some tap trial setup

---
+++ Assets/Scripts/Games/TapTrial/TapTrial.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RhythmHeavenMania.Games.TapTrial
+{
+    public class TapTrial : Minigame
+    {
+    }
+}
