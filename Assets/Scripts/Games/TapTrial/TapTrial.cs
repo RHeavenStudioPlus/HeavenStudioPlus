538bc3cfa5510df52a3b112ddabb7266d3eddc5b
Carson Kompon <carsokompo@gmail.com>
2022-02-27 19:17:12 -0500

    Added Placeholder animations for Tap Trial
    Added Double Tap and Triple Tap placeholder animations so they can be used in a remix until they're made actually playable.

--- Assets/Scripts/Games/TapTrial/TapTrial.cs
+++ Assets/Scripts/Games/TapTrial/TapTrial.cs
@@ -18,6 +18,8 @@ namespace RhythmHeavenMania.Games.TapTrial
             instance = this;
         }
 
+        // The following is all solely animations for placeholder. This isn't playable
+
         public void Tap(float beat)
         {
             MultiSound.Play(new MultiSound.Sound[]
@@ -37,12 +39,44 @@ namespace RhythmHeavenMania.Games.TapTrial
 
         public void DoubleTap(float beat)
         {
+            MultiSound.Play(new MultiSound.Sound[]
+            {
+                new MultiSound.Sound("tapTrial/ookook",   beat),
+                new MultiSound.Sound("tapTrial/ookook",   beat + 0.5f),
+                new MultiSound.Sound("tapTrial/tap",      beat + 1.0f),
+                new MultiSound.Sound("tapTrial/tap",      beat + 1.5f),
+            });
 
+            GameObject beatAction = new GameObject();
+            beatAction.transform.SetParent(this.transform);
+            BeatAction.New(beatAction, new List<BeatAction.Action>()
+            {
+                new BeatAction.Action(beat + 0.0f, delegate { player.anim.Play("DoubleTapPrepare", 0, 0); }),
+                new BeatAction.Action(beat + 1.0f, delegate { player.anim.Play("DoubleTap", 0, 0); }),
+                new BeatAction.Action(beat + 1.5f, delegate { player.anim.Play("DoubleTap", 0, 0); }),
+            });
         }
 
         public void TripleTap(float beat)
         {
+            MultiSound.Play(new MultiSound.Sound[]
+            {
+                new MultiSound.Sound("tapTrial/ooki1",   beat),
+                new MultiSound.Sound("tapTrial/ooki2",   beat + 0.5f),
+                new MultiSound.Sound("tapTrial/tap",      beat + 2.0f),
+                new MultiSound.Sound("tapTrial/tap",      beat + 2.5f),
+                new MultiSound.Sound("tapTrial/tap",      beat + 3.0f),
+            });
 
+            GameObject beatAction = new GameObject();
+            beatAction.transform.SetParent(this.transform);
+            BeatAction.New(beatAction, new List<BeatAction.Action>()
+            {
+                new BeatAction.Action(beat + 0.0f, delegate { player.anim.Play("PosePrepare", 0, 0); }),
+                new BeatAction.Action(beat + 2.0f, delegate { player.anim.Play("Tap", 0, 0); }),
+                new BeatAction.Action(beat + 2.5f, delegate { player.anim.Play("DoubleTap", 0, 0); }),
+                new BeatAction.Action(beat + 3.0f, delegate { player.anim.Play("Tap", 0, 0); }),
+            });
         }
 
         public void JumpTap(float beat)
