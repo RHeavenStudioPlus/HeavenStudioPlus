a2e6379528a8b7c171ff68902cdf7f02d06af71d
Carson Kompon <carsokompo@gmail.com>
2022-03-03 18:33:22 -0500

    Began adding Wizard's Waltz

---
+++ Assets/Scripts/Games/WizardsWaltz/WizardsWaltz.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RhythmHeavenMania.Util;
+
+namespace RhythmHeavenMania.Games.WizardWaltz
+{
+    public class WizardsWaltz : Minigame
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
