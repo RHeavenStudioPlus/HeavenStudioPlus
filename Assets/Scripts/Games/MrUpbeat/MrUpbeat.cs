d22009e79c1781b637726d595ce8cdcb61cbef95
Carson Kompon <carsokompo@gmail.com>
2022-03-04 17:03:57 -0500

    Mr Upbeat Programming progress
    Autoplay works but it doesn't work otherwise

--- Assets/Scripts/Games/MrUpbeat/MrUpbeat.cs
+++ Assets/Scripts/Games/MrUpbeat/MrUpbeat.cs
@@ -12,9 +12,11 @@ namespace RhythmHeavenMania.Games.MrUpbeat
     {
         [Header("References")]
         public GameObject metronome;
-        public Animator animator;
-        public Animator blipAnimator;
-        public GameObject[] shadows;
+        public UpbeatMan man;
+
+        public float nextBeat;
+        public bool canGo = false;
+
 
         public static MrUpbeat instance;
 
@@ -23,6 +25,29 @@ namespace RhythmHeavenMania.Games.MrUpbeat
             instance = this;
         }
 
+        private void Update()
+        {
+            List<Beatmap.Entity> gos = GameManager.instance.Beatmap.entities.FindAll(c => c.datamodel == "mrUpbeat/go");
+            for(int i=0; i<gos.Count; i++)
+            {
+                if ((gos[i].beat - 0.15f) <= Conductor.instance.songPositionInBeats && (gos[i].beat + gos[i].length) - 0.15f > Conductor.instance.songPositionInBeats)
+                {
+                    canGo = true;
+                    break;
+                } else
+                {
+                    canGo = false;
+                }
+            }
+
+            float normalizedBeat = Conductor.instance.GetPositionFromBeat(nextBeat, 0.5f);
+            //StateCheck(normalizedBeat);
+        }
+
+        public void SetInterval(float beat)
+        {
+            nextBeat = beat;
+        }
        
 
     }
