95a04168ecfba1097f0dc1b388c135961ebda3ee
Jenny Crowe <jakobwcrowe@berkeley.edu>
2022-03-13 07:35:22 -0700

    Fireworks: More setup

---
+++ Assets/Scripts/Games/Fireworks/Fireworks.cs
@@ -0,0 +1,23 @@
+using RhythmHeavenMania.Util;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RhythmHeavenMania.Games
+{
+    using Scripts_Fireworks;
+    public class Fireworks : Minigame
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
