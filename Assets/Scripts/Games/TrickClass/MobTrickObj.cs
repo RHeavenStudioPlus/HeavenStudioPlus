035d3f4fce283dc2fbeeba14f03ba14435744209
minenice55 <star.elementa@gmail.com>
2022-04-10 18:29:31 -0400

    Trick on the Class: flying objects

--- Assets/Scripts/Games/TrickClass/MobTrickObj.cs
+++ Assets/Scripts/Games/TrickClass/MobTrickObj.cs
@@ -23,12 +23,30 @@ namespace HeavenStudio.Games.Scripts_TrickClass
         private void Awake()
         {
             game = TrickClass.instance;
+            flyBeats = flyType ? 4f : 2f;
+
+            var cond = Conductor.instance;
+
+            float flyPos = cond.GetPositionFromBeat(startBeat, flyBeats);
+            transform.position = curve.GetPoint(flyPos);
         }
 
         // Update is called once per frame
         void Update()
         {
-            
+            if (flying)
+            {
+                var cond = Conductor.instance;
+
+                float flyPos = cond.GetPositionFromBeat(startBeat, flyBeats);
+                transform.position = curve.GetPoint(flyPos);
+
+                if (flyPos > 1f)
+                {
+                    GameObject.Destroy(gameObject);
+                    return;
+                }
+            }
         }
     }
 }
