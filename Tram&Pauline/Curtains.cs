63d108cde3e01694f0a4e83463d8031541713385
adianfiuef <103307721+adianfiuef@users.noreply.github.com>
2022-06-21 18:31:20 -0700

    Tram&Pauline
    Update

---
+++ Tram&Pauline/Curtains.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using HeavenStudio.Util;
+
+namespace HeavenStudio.Games.Scripts_TramAndPauline
+{
+    public class Curtains : MonoBehaviour
+    {
+        private Animator anim;
+
+
+        private void Awake()
+        {
+
+        }
+    }
+}
+    
+
+
+
+
+
+
+  
