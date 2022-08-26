603067c35db7a67e2b93026862e643e7b639bcfd
KrispyDotlessI <92911290+KrispyDotlessI@users.noreply.github.com>
2022-08-26 16:29:26 +0800

    Sarge placement, yet to animate
    time to waste time on creating a minigame rather than actually modding megamix to make a custom remix :ok_hand:

--- Assets/Scripts/Games/MarchingOrders/MarchingOrders.cs
+++ Assets/Scripts/Games/MarchingOrders/MarchingOrders.cs
@@ -1,3 +1,10 @@
+//notes:
+//1. see space soccer, mr upbeat, tunnel for keep-the-beat codes
+//2. see fan club for separate prefabs
+//3. temporarily take sounds from rhre, wait until someone records the full code, including misses, or record it myself (unlikely)
+//4. figure how to do custom bg changes when the upscaled textures are finished (see karate man, launch party once it releases)
+//5. delete all notes once the minigame is considered feature-complete
+
 using HeavenStudio.Util;
 using System;
 using System.Collections.Generic;
@@ -20,7 +27,7 @@ namespace HeavenStudio.Games.Loaders
 					
 					
 					//the cues do nothing at the moment, so i temporarily disabled them
-					//new GameAction("marching", delegate {}, 4f, true),
+					//new GameAction("marching", delegate { MarchingOrders.instance.CadetsMarch(eventCaller.currentEntity.beat); }, 4f, true),
 					
 					//new GameAction("attention", delegate {}, 2f, false),
 					//new GameAction("march", delegate {}, 2f, false),
@@ -28,7 +35,7 @@ namespace HeavenStudio.Games.Loaders
 					//new GameAction("face turn", delegate {}, 4f, false, parameters: new List<Param>()
 					//{
                     //    new Param("type", MarchingOrders.DirectionFaceTurn.Right, "Direction", "The direction sarge wants the cadets to face"),
-					//	new Param("type2", MarchingOrders.FaceTurnLength.Normal, "Length", "How fast or slow the event lasts"),
+					//  	new Param("type2", MarchingOrders.FaceTurnLength.Normal, "Length", "How fast or slow the event lasts"),
                     //}),
                 });
         }
@@ -49,10 +56,12 @@ namespace HeavenStudio.Games
             Fast,
 		}
 		
+		public static MarchingOrders instance;
+		
         // Start is called before the first frame update
         void Awake()
         {
-            
+            instance = this;
         }
 
         // Update is called once per frame
@@ -60,6 +69,11 @@ namespace HeavenStudio.Games
         {
             
         }
+		
+		public void CadetsMarch(float beat)
+		{
+			
+		}
     }
 }
 
