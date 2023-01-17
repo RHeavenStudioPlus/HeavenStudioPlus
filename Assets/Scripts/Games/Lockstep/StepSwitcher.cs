8d707ecd4e44367bd8fd24533b8ce62a5449adaa
RaffyTaffy14 <105959219+RaffyTaffy14@users.noreply.github.com>
2023-01-16 23:29:05 -0500

    Lockstep Initialization (#203)
    * Lockstep
    
    fansodnason
    
    * More Lockstep
    
    Implemented the new sprites (still needs work). Added new Icon.
    
    * More lockstep stuff
    
    i have no clue
    
    * Animation Tweaking
    
    I fiddled around with numbers weeeeeeeeeeeeeeee

---
+++ Assets/Scripts/Games/Lockstep/StepSwitcher.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using HeavenStudio.Util;
+
+namespace HeavenStudio.Games.Scripts_Lockstep
+{
+    public class StepSwitcher : PlayerActionObject
+    {
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
