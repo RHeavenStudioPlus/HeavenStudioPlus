f0838444a878c48a9b6e86f1e318ea64a3008a25
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-01-19 18:18:55 +0100

    Add Working Dough (Finished), Missing HD Assets and Updated Icon only. (#210)
    * Added sfx, sprites and folders neccesary. And also made an empty game object for working dough
    
    * Started work on the background
    
    * Implemented all unanimated sprites
    
    * Added Init
    
    * Added Prefabs
    
    * Added Jumping and Idle Animatins for the Dough Dudes
    
    * SmallFix
    
    * Start Interval Event Implemented
    
    * Added Audio and Small and Big Ball events
    
    * Code Improvement + starting making the balls
    
    * Added bezier curves
    
    * Added First Rendition of Balls
    
    * Added NPC Balls and starting doing small transporting animations
    
    * SmallFixes
    
    * SmallSoundChanges
    
    * Implemented Audio and prefunction, not done with it though
    
    * In process of fixing set interval
    
    * Added more prefunction stuff and also started working on player input
    
    * Tried adding player balls, not done, many bugs to fix
    
    * Reverted trying to make the playerenterdoughball handle inputs, gonna do it through the main script instead
    
    * Sat up input code for later
    
    * Input works now! Need to add barely and wrong input animations and fix bugs
    
    * Tiny fix
    
    * Added first draft of wronginput animations
    
    * Finished all input anims, trying to fix bugs
    
    * Added finished Spaceship animations
    
    * Added Mr Game and Watch WIP and lifting dough dudes
    
    * Finished GANDW, fixed some bugs and still fixing bugs, almost done!
    
    * DONE

---
+++ Assets/Scripts/Games/WorkingDough/BGBall.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyBezierCurves;
+using HeavenStudio.Util;
+
+namespace HeavenStudio.Games.Scripts_WorkingDough
+{
+    public class BGBall : PlayerActionObject
+    {
+        public float startBeat;
+        public float firstBeatsToTravel = 3f;
+        public float secondBeatsToTravel = 1f;
+        public float thirdBeatsToTravel = 3f;
+        public enum CurveStage
+        {
+            Conveyer = 0,
+            StartFall = 1,
+            Fall = 2
+        }
+        public CurveStage currentCurveStage;
+        [NonSerialized] public BezierCurve3D firstCurve;
+        [NonSerialized] public BezierCurve3D secondCurve;
+        [NonSerialized] public BezierCurve3D thirdCurve;
+
+        private void Update()
+        {
+            var cond = Conductor.instance;
+
+            float flyPos = 0f;
+
+            switch (currentCurveStage)
+            {
+                case CurveStage.Conveyer:
+                    flyPos = cond.GetPositionFromBeat(startBeat, firstBeatsToTravel);
+                    transform.position = firstCurve.GetPoint(flyPos);
+                    if (flyPos > 1f)
+                    {
+                        currentCurveStage = CurveStage.StartFall;
+                    }
+                    break;
+                case CurveStage.StartFall:
+                    flyPos = cond.GetPositionFromBeat(startBeat + firstBeatsToTravel, secondBeatsToTravel);
+                    transform.position = secondCurve.GetPoint(flyPos);
+                    if (flyPos > 1f) currentCurveStage = CurveStage.Fall;
+                    break;
+                case CurveStage.Fall:
+                    flyPos = cond.GetPositionFromBeat(startBeat + secondBeatsToTravel + firstBeatsToTravel, thirdBeatsToTravel);
+
+                    transform.position = thirdCurve.GetPoint(flyPos);
+                    if (flyPos > 1f) GameObject.Destroy(gameObject);
+                    break;
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Games/WorkingDough/PlayerEnterDoughBall.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyBezierCurves;
+using HeavenStudio.Util;
+
+namespace HeavenStudio.Games.Scripts_WorkingDough
+{
+    public class PlayerEnterDoughBall : PlayerActionObject
+    {
+        public float startBeat;
+        public float firstBeatsToTravel = 0.5f;
+        public float secondBeatsToTravel = 0.5f;
+        public bool goingDown = false;
+        public bool deletingAutomatically = true;
+        [NonSerialized] public BezierCurve3D firstCurve;
+        [NonSerialized] public BezierCurve3D secondCurve;
+
+        private void Update()
+        {
+            var cond = Conductor.instance;
+
+            float flyPos = 0f;
+
+            if (goingDown)
+            {
+                flyPos = cond.GetPositionFromBeat(startBeat + firstBeatsToTravel, secondBeatsToTravel);
+
+                transform.position = secondCurve.GetPoint(flyPos);
+                if (flyPos > 1f) if (deletingAutomatically) GameObject.Destroy(gameObject);
+            }
+            else
+            {
+                flyPos = cond.GetPositionFromBeat(startBeat, firstBeatsToTravel);
+                transform.position = firstCurve.GetPoint(flyPos);
+                if (flyPos > 1f) goingDown = true;
+            }
+        }
+    }
+}
+
+
