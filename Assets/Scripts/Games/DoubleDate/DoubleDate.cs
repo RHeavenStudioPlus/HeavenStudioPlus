8ccced19b6dcf71c152bc0b2e1c3987f7d2fc3ca
ev <85412919+evdial@users.noreply.github.com>
2023-03-26 23:12:23 -0400

    Double Date (#366)
    * Got the inputs and audio in
    
    * Fixed some things
    
    * kick anim
    
    * more stuff
    
    * anim stuff
    
    * particle mistake oops
    
    * Oops. I am sorry
    
    * Implemented visual stuff
    
    * Made animations scaled to be faster
    
    * Fixes
    
    * lots of anims
    
    * More anims implemented
    
    ---------
    
    Co-authored-by: Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
    Co-authored-by: minenice55 <star.elementa@gmail.com>

--- Assets/Scripts/Games/DoubleDate/DoubleDate.cs
+++ Assets/Scripts/Games/DoubleDate/DoubleDate.cs
@@ -9,23 +9,33 @@ namespace HeavenStudio.Games.Loaders
     public static class RvlDoubleDateLoader
     {
         public static Minigame AddGame(EventCaller eventCaller) {
-            return new Minigame("doubleDate", "Double Date \n<color=#eb5454>[INITIALIZATION ONLY]</color>", "ef854a", false, false, new List<GameAction>()
+            return new Minigame("doubleDate", "Double Date", "ef854a", false, false, new List<GameAction>()
             {
-                new GameAction("soccerBall", "Soccer Ball")
+                new GameAction("bop", "Bop")
                 {
-                    function = delegate { var e = eventCaller.currentEntity; DoubleDate.instance.ball(e.beat, e["type"]); }, 
-                    defaultLength = 2,
+                    function = delegate { var e = eventCaller.currentEntity; DoubleDate.instance.Bop(e.beat, e.length, e["bop"], e["autoBop"]); },
+                    resizable = true,
+                    parameters = new List<Param>()
+                    {
+                        new Param("bop", true, "Bop", "Should the two couples bop?"),
+                        new Param("autoBop", false, "Bop (Auto)", "Should the two couples auto bop?")
+                    }
                 },
-                new GameAction("basketball", "Basketball")
+                new GameAction("soccer", "Soccer Ball")
                 {
-                    function = delegate { var e = eventCaller.currentEntity; DoubleDate.instance.ball(e.beat, e["type"]); }, 
-                    defaultLength = 2,
+                    function = delegate { var e = eventCaller.currentEntity; DoubleDate.instance.SpawnSoccerBall(e.beat); },
+                    defaultLength = 2f,
+                },
+                new GameAction("basket", "Basket Ball")
+                {
+                    function = delegate { var e = eventCaller.currentEntity; DoubleDate.instance.SpawnBasketBall(e.beat); },
+                    defaultLength = 2f,
                 },
                 new GameAction("football", "Football")
                 {
-                    function = delegate { var e = eventCaller.currentEntity; DoubleDate.instance.ball(e.beat, e["type"]); }, 
-                    defaultLength = 2,
-                }
+                    function = delegate { var e = eventCaller.currentEntity; DoubleDate.instance.SpawnFootBall(e.beat); },
+                    defaultLength = 2.5f,
+                },
             });
         }
     }
@@ -37,27 +47,126 @@ namespace HeavenStudio.Games
 
     public class DoubleDate : Minigame
     {
+        [Header("Prefabs")]
+        [SerializeField] SoccerBall soccer;
+        [SerializeField] Basketball basket;
+        [SerializeField] Football football;
+        [SerializeField] GameObject leaves;
+        [Header("Components")]
+        [SerializeField] Animator boyAnim;
+        [SerializeField] Animator girlAnim;
+        [SerializeField] DoubleDateWeasels weasels;
+        [SerializeField] Animator treeAnim;
+        [Header("Variables")]
+        bool shouldBop = true;
+        bool canBop = true;
+        GameEvent bop = new GameEvent();
         public static DoubleDate instance;
-
-        [Header("Objects")]
-        public Animator soccerBallAnim;
-        public Animator basketballAnim;
-        public Animator footballAnim;
         
         private void Awake()
         {
             instance = this;
         }
 
-        private void HitSound(bool applause)
+        void Update()
         {
-            Jukebox.PlayOneShotGame("doubleDate/kick");
-            if (applause) Jukebox.PlayOneShot("applause");
+            var cond = Conductor.instance;
+            if (cond.isPlaying && !cond.isPaused)
+            {
+                if (cond.ReportBeat(ref bop.lastReportedBeat, bop.startBeat % 1) && shouldBop)
+                {
+                    SingleBop();
+                }
+            }
+            if (PlayerInput.Pressed() && !IsExpectingInputNow(InputType.STANDARD_DOWN))
+            {
+                Jukebox.PlayOneShotGame("doubleDate/kick_whiff");
+                Kick(true, true, false);
+            }
         }
 
-        public void ball(float beat, int type)
+        public void ToggleBop(bool go)
         {
-            Jukebox.PlayOneShotGame("doubleDate/soccerBall");
+            canBop = go;
+        }
+
+        public void Bop(float beat, float length, bool goBop, bool autoBop)
+        {
+            shouldBop = autoBop;
+            if (goBop)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    BeatAction.New(instance.gameObject, new List<BeatAction.Action>()
+                    {
+                        new BeatAction.Action(beat + i, delegate { SingleBop(); })
+                    });
+                }
+            }
+        }
+
+        void SingleBop()
+        {
+            if (canBop)
+            {
+                boyAnim.DoScaledAnimationAsync("IdleBop", 1f);
+            }
+            girlAnim.DoScaledAnimationAsync("GirlBop", 1f);
+            weasels.Bop();
+        }
+
+        public void Kick(bool hit = true, bool forceNoLeaves = false, bool weaselsHappy = true)
+        {
+            if (hit)
+            {
+                boyAnim.DoScaledAnimationAsync("Kick", 1f);
+                if (weaselsHappy) weasels.Happy();
+                if (!forceNoLeaves)
+                {
+                    BeatAction.New(instance.gameObject, new List<BeatAction.Action>()
+                    {
+                        new BeatAction.Action(Conductor.instance.songPositionInBeats + 1f, delegate
+                        {
+                            GameObject spawnedLeaves = Instantiate(leaves, transform);
+                            spawnedLeaves.SetActive(true);
+                            treeAnim.DoScaledAnimationAsync("TreeRustle", 1f);
+                        })
+                    });
+                }
+            }
+            else
+            {
+                boyAnim.DoScaledAnimationAsync("Barely", 1f);
+            }
+        }
+
+        public void SpawnSoccerBall(float beat)
+        {
+            SoccerBall spawnedBall = Instantiate(soccer, instance.transform);
+            spawnedBall.Init(beat);
+            Jukebox.PlayOneShotGame("doubleDate/soccerBounce", beat);
+        }
+
+        public void SpawnBasketBall(float beat)
+        {
+            Basketball spawnedBall = Instantiate(basket, instance.transform);
+            spawnedBall.Init(beat);
+            MultiSound.Play(new MultiSound.Sound[]
+            {
+                new MultiSound.Sound("doubleDate/basketballBounce", beat),
+                new MultiSound.Sound("doubleDate/basketballBounce", beat + 0.75f),
+            });
+        }
+
+        public void SpawnFootBall(float beat)
+        {
+            Football spawnedBall = Instantiate(football, instance.transform);
+            spawnedBall.Init(beat);
+            MultiSound.Play(new MultiSound.Sound[]
+            {
+                new MultiSound.Sound("doubleDate/footballBounce", beat),
+                new MultiSound.Sound("doubleDate/footballBounce", beat + 0.75f),
+            });
         }
     }
 
--- Assets/Scripts/Games/DoubleDate/Football.cs
+++ Assets/Scripts/Games/DoubleDate/Football.cs
@@ -8,18 +8,44 @@ namespace HeavenStudio.Games.Scripts_DoubleDate
 {
     public class Football : PlayerActionObject
     {
-        public bool isSparkler;
+        private DoubleDate game;
 
-        // Start is called before the first frame update
         void Awake()
         {
-            
+            game = DoubleDate.instance;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void Init(float beat)
         {
-            
+            game.ScheduleInput(beat, 1.5f, InputType.STANDARD_DOWN, Just, Miss, Empty);
         }
+
+        void Just(PlayerActionEvent caller, float state)
+        {
+            if (state >= 1f || state <= -1f)
+            {
+                Jukebox.PlayOneShot("miss");
+                game.Kick(false);
+                Destroy(gameObject); //Remove this when doing the ball movement
+                return;
+            }
+            Hit();
+        }
+
+        void Hit()
+        {
+            game.Kick(true, true);
+            Jukebox.PlayOneShotGame("doubleDate/footballKick");
+            Destroy(gameObject); //Remove this when doing the ball movement
+        }
+
+        void Miss(PlayerActionEvent caller)
+        {
+            Jukebox.PlayOneShotGame("doubleDate/weasel_hit");
+            Jukebox.PlayOneShotGame("doubleDate/weasel_scream");
+            Destroy(gameObject); //Remove this when doing the ball movement
+        }
+
+        void Empty(PlayerActionEvent caller) { }
     }
 }
