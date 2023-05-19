2d76707a8dff5dd53ffa85a4857fca4489127214
minenice55 <star.elementa@gmail.com>
2023-05-19 19:10:34 -0400

    remove reference to internal assembly (#425)

--- Assets/Scripts/Games/TossBoys/TossBoysBall.cs
+++ Assets/Scripts/Games/TossBoys/TossBoysBall.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using HeavenStudio.Util;
-using UnityEditorInternal;
 using System;
 
 namespace HeavenStudio.Games.Scripts_TossBoys
