bbca7457cc79817ce3dbe3fcb0f47eff257edd71
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-05-20 00:21:02 +0200

    Toss Boys (#424)
    * sprites and stuff lolol
    
    * oops
    
    * Real
    
    * akahitanim
    
    * lots of anims done
    
    * more anims
    
    * many anims many sprites
    
    * LOLLL OOPS
    
    * oops 2
    
    * all anims done
    
    * oops2
    
    * oops3
    
    * special overlay
    
    * Basics
    
    * Inputs stuff
    
    * Basic pass ball implemented
    
    * Transitions :fire:
    
    * Nvm transitions are gay
    
    * Dual toss added
    
    * Idiot proof toss boys
    
    * prepare anims
    
    * Some special logic
    
    * Done with special for dual toss and added bops
    
    * little anim tweak
    
    * Added pop ball and fixed some things
    
    * Funny sound offsets
    
    * Added high toss + fixed a bug
    
    * icons and more animations
    
    * epic insane particle changes (CRAZY!!!!!!!!!!!)
    
    * FIXES!
    
    * Added call and fixed a bug
    
    * Panning added
    
    * Lightning toss added
    
    * Blur toss Added
    
    * Added fade in for the special overlay and fixed a bug
    
    * cool animation shit
    
    * animations and sprites fully done
    
    * set up basic curves
    
    * The toss boys main script just needs to set the state of the ball now
    
    * All curves implemented - untested though
    
    * Smol fix!
    
    * slightly new sheet
    
    * Bg Recolor + ball anims + whiff cooldown
    
    * Specials now properly handle themselves when chained together
    
    * ball anims
    
    * Fixes and tweaks
    
    * Fixed a bug with lightning toss
    
    * small tweaks
    
    * Only thing toss boys needs is the pitch warble now
    
    ---------
    
    Co-authored-by: ev <85412919+evdial@users.noreply.github.com>

---
+++ Assets/Scripts/Games/TossBoys/TossKid.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HeavenStudio.Util;
+
+namespace HeavenStudio.Games.Scripts_TossBoys
+{
+    public class TossKid : MonoBehaviour
+    {
+        [SerializeField] ParticleSystem _hitEffect;
+        [SerializeField] GameObject arrow;
+        Animator anim;
+        [SerializeField] string prefix;
+        TossBoys game;
+        public bool crouch;
+        bool preparing;
+
+        private void Awake()
+        {
+            anim = GetComponent<Animator>();
+            game = TossBoys.instance;
+            DoAnimationScaledAsync("Idle", 20f);
+        }
+
+        public void HitBall(bool hit = true)
+        {
+            if (hit)
+            {
+                ParticleSystem spawnedEffect = Instantiate(_hitEffect, transform);
+                spawnedEffect.Play();
+                DoAnimationScaledAsync(crouch ? "CrouchHit" : "Hit", 0.5f);
+            }
+            else if (!anim.IsPlayingAnimationName(prefix + "Whiff") && !anim.IsPlayingAnimationName(prefix + "Miss"))
+            {
+                DoAnimationScaledAsync("Whiff", 0.5f);
+                Jukebox.PlayOneShotGame("tossBoys/whiff");
+            }
+        }
+
+        public void Bop()
+        {
+            if (!anim.IsAnimationNotPlaying() || crouch || preparing) return;
+            DoAnimationScaledAsync("Bop", 0.5f);
+        }
+
+        public void Crouch()
+        {
+            DoAnimationScaledAsync("Crouch", 0.5f);
+            crouch = true;
+        }
+
+        public void PopBall()
+        {
+            DoAnimationScaledAsync("Slap", 0.5f);
+            preparing = false;
+        }
+
+        public void PopBallPrepare()
+        {
+            if (preparing) return;
+            DoAnimationScaledAsync("PrepareHand", 0.5f);
+            preparing = true;
+        }
+
+        public void Miss()
+        {
+            DoAnimationScaledAsync("Miss", 0.5f);
+        }
+
+        public void Barely()
+        {
+            DoAnimationScaledAsync("Barely", 0.5f);
+        }
+
+        public void ShowArrow(float startBeat, float length)
+        {
+            BeatAction.New(game.gameObject, new List<BeatAction.Action>(){
+                new BeatAction.Action(startBeat, delegate { arrow.SetActive(true); }),
+                new BeatAction.Action(startBeat + length, delegate { arrow.SetActive(false); }),
+            });
+        }
+
+        void DoAnimationScaledAsync(string name, float time)
+        {
+            anim.DoScaledAnimationAsync(prefix + name, time);
+        }
+    }
+}
+
