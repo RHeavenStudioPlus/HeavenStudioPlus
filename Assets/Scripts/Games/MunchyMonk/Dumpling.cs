2c1394feb6e6a3bf5f14ce5867b99263f61fb297
AstrlJelly <bdlawson115@gmail.com>
2023-05-23 00:09:46 -0400

    Munchy Monk Finalized (#432)
    * stache stuff (still needs more work but yeah)
    
    * new icon (TEMPORARY ICON!!!)
    * some placeholder animations
    * more two two jank fixed, almost there
    
    * superscroll + fixing early misses
    
    * made a few temporary changes to superscroll.cs, i will be modifying it further for ease of use but for now it isn't permanent
    * made real progress on stopping repeat inputs
     -game is basically broken for the time being, but i wanna push this so nothing theoretically breaks. ill fix it all tomorrow when im less tired
    
    * it's been a while since i've committed my changes
    
    * modifiers work again (idk if i broke that in this commit or it was already broken)
    * stacking logic is here!!! it always uses the latest objects' hit/miss/early function so it should be perfect. i might be able to improve on efficiency tho
     -FollowHand animation is broken again unfortunately :(
    * dumplings can now be individually colored, and are assigned their colors in Start(). the smear's colors are defined on Hit() (HitFunction() to be specific) , so that even when there are multiple stacked dumplings, they'll all be accurate.
    
    maybe some other stuff but i cannot remember
    
    * clean-up + integral fixes
    
    onto Monk Move and SingleSuperScroll
    i think ill add some more comments too
    
    * fixed some missing/early stuff + a check for whether you can hit or not
    * fixed FollowHand animation and the squishing/dumpling on top
    * cleaned up a ton of unused variables + checks + anims
    * added unused sound sequences
     -ig if somebody knows how to use them they can implement them but idk how to and it's not worth figuring out rn
    * more work on superscroll; there seems to be too many changes to the structure of the script to keep the single scrolling in with the tile scrolling, will combine if necessary tho
    * fanClub/arisa_dab
    
    * monk move!!
    
    still working on singlesuperscroll :sob:
    but i got the monk moving!!
    
    * a few optimizations + starting on new singlesuperscroll
    
    * ok ScrollObject is usable now
    
    oh my GOD i need to optimize how it looks and functions but rn it works like it will when it's done (theoretically)
    ev do ur work
    
    * a few things + new icon!!
    
    * scroll speeds are good now
    
    * finished except for the stache bop
    
    :sob: :sob: :sob: :sob: :sob: why isn't it working
    
    * im pring this now
    
    ---------
    
    Co-authored-by: ev <85412919+evdial@users.noreply.github.com>

--- Assets/Scripts/Games/MunchyMonk/Dumpling.cs
+++ Assets/Scripts/Games/MunchyMonk/Dumpling.cs
@@ -9,70 +9,72 @@ namespace HeavenStudio.Games.Scripts_MunchyMonk
 {
     public class Dumpling : MonoBehaviour
     {
-        public Animator otherAnim;
+        public Color dumplingColor;
         public float startBeat;
-        public float type;
+        
         const string sfxName = "munchyMonk/";
-        private bool canDestroy;
+        public bool canDestroy;
         
         [Header("References")]
         [SerializeField] Animator smearAnim;
         [SerializeField] Animator anim;
+        [SerializeField] SpriteRenderer smearSr;
+        public SpriteRenderer sr;
 
         private MunchyMonk game;
 
         private void Awake()
         {
             game = MunchyMonk.instance;
-            game.firstTwoMissed = false;
+            sr = GetComponent<SpriteRenderer>();
         }
 
         private void Start() 
         {
-            if (type == 1f || type == 3f) {
-                game.ScheduleInput(startBeat, 1f, InputType.STANDARD_DOWN, Hit, Miss, Early);
-            } else if (type >= 3.5f) {
-                game.ScheduleInput(startBeat, 0.75f, InputType.STANDARD_DOWN, Hit, Miss, Early);
-            } else {
-                game.ScheduleInput(startBeat, type == 2f ? 1.5f : 2f, InputType.STANDARD_DOWN, Hit, Miss, Early);
+            sr.color = dumplingColor;
+            if (game.dumplings.Count > 1) {
+                anim.Play("IdleOnTop", 0, 0);
+                game.dumplings[0].anim.DoScaledAnimationAsync("Squish", 0.5f);
             }
         }
 
         private void Update()
         {
-            if ((!Conductor.instance.isPlaying && !Conductor.instance.isPaused) || GameManager.instance.currentGame != "munchyMonk") {
-                GameObject.Destroy(gameObject);
-            }
-
             if (canDestroy && anim.IsAnimationNotPlaying()) GameObject.Destroy(gameObject);
         }
 
-        private void Hit(PlayerActionEvent caller, float state)
+        public void HitFunction(float state)
         {
-            if (!canDestroy) {
-                game.MonkArmsAnim.DoScaledAnimationAsync("WristSlap", 0.5f);
-                Jukebox.PlayOneShotGame(sfxName+"slap");
-                game.isStaring = false;
-                
-                if (state >= 1f || state <= -1f) 
+            smearSr.color = dumplingColor;
+            game.MonkArmsAnim.DoScaledAnimationAsync("WristSlap", 0.5f);
+            Jukebox.PlayOneShotGame(sfxName+"slap");
+            game.isStaring = false;
+            
+            if (state >= 1f || state <= -1f)
+            {
+                game.MonkAnim.DoScaledAnimationAsync("Barely", 0.5f);
+                anim.DoScaledAnimationAsync("HitHead", 0.5f);
+                Jukebox.PlayOneShotGame(sfxName+"barely");
+                canDestroy = true;
+                game.needBlush = false;
+            } else {
+                game.MonkAnim.DoScaledAnimationAsync("Eat", 0.4f);
+                game.dumplings[0].anim.DoScaledAnimationAsync("FollowHand", 0.5f);
+                smearAnim.Play("SmearAppear", 0, 0);
+                game.needBlush = true;
+                Jukebox.PlayOneShotGame(sfxName+"gulp");
+                MunchyMonk.howManyGulps++;
+                for (int i = 1; i <= 4; i++)
                 {
-                    game.MonkAnim.DoScaledAnimationAsync("Barely", 0.5f);
-                    anim.DoScaledAnimationAsync("HitHead", 0.5f);
-                    Jukebox.PlayOneShotGame(sfxName+"barely");
-                    canDestroy = true;
-                } else {
-                    game.MonkAnim.DoScaledAnimationAsync("Eat", 0.4f);
-                    if (type == 2) otherAnim.DoScaledAnimationAsync("FollowHand", 0.5f);
-                    smearAnim.Play("SmearAppear", 0, 0);
-                    game.needBlush = true;
-                    Jukebox.PlayOneShotGame(sfxName+"gulp");
-                    if (game.forceGrow) game.growLevel += 1;
-                    GameObject.Destroy(gameObject);
+                    if (MunchyMonk.howManyGulps == MunchyMonk.inputsTilGrow*i) {
+                        MunchyMonk.growLevel = i;
+                    }
                 }
+                GameObject.Destroy(gameObject);
             }
         }
 
-        private void Miss(PlayerActionEvent caller)
+        public void MissFunction()
         {
             if (!canDestroy) {
                 anim.DoScaledAnimationAsync("FallOff", 0.5f);
@@ -80,21 +82,18 @@ namespace HeavenStudio.Games.Scripts_MunchyMonk
             }
         }
 
-        private void Early(PlayerActionEvent caller) 
-        { 
-            if (!(type == 2.5f) || game.firstTwoMissed) {
-                game.MonkArmsAnim.DoScaledAnimationAsync("WristSlap", 0.5f);
-                game.MonkAnim.DoScaledAnimationAsync("Miss", 0.5f);
-                smearAnim.Play("SmearAppear", 0, 0);
-                anim.DoScaledAnimationAsync("HitHead", 0.5f);
-                MultiSound.Play(new MultiSound.Sound[] {
-                    new MultiSound.Sound(sfxName+"slap", game.lastReportedBeat),
-                    new MultiSound.Sound(sfxName+"miss", game.lastReportedBeat),
-                });
-                canDestroy = true;
-                game.needBlush = false;
-                game.firstTwoMissed = true;
-            }
+        public void EarlyFunction()
+        {
+            game.MonkArmsAnim.DoScaledAnimationAsync("WristSlap", 0.5f);
+            game.MonkAnim.DoScaledAnimationAsync("Miss", 0.5f);
+            smearAnim.Play("SmearAppear", 0, 0);
+            anim.DoScaledAnimationAsync("HitHead", 0.5f);
+            MultiSound.Play(new MultiSound.Sound[] {
+                new MultiSound.Sound(sfxName+"slap", Conductor.instance.songPositionInBeats),
+                new MultiSound.Sound(sfxName+"miss", Conductor.instance.songPositionInBeats),
+            });
+            canDestroy = true;
+            game.needBlush = false;
         }
     }
 }
