878e07ae3193f9101d594cb68a287d6632f16eee
minenice55 <star.elementa@gmail.com>
2023-11-24 17:59:34 -0500

    Tunnel tunnels (#577)
    * tunnel tunnel
    
    tunnel
    
    * tunnel messes with the volume
    
    * tempo finder can now be reset by waiting
    
    now uses the conductor's time source if it exists and is playing
    
    * wip anims
    
    * Animations Finished
    
    * add tunnel sound
    
    * tunnel shader
    
    fix the left sound
    
    * add pre-sliced BG assets
    
    ---------
    
    Co-authored-by: Seanski2 <seanbenedit@gmail.com>

--- Assets/Scripts/AppInfo.cs
+++ Assets/Scripts/AppInfo.cs
@@ -1,8 +1,8 @@
 using System;
 
 public static class AppInfo {
-    public const string Version = "0.0.1016";
-    public static readonly DateTime Date = new DateTime(2023, 10, 20, 02, 31, 04, 288, DateTimeKind.Utc);
+    public const string Version = "0.0.1017";
+    public static readonly DateTime Date = new DateTime(2023, 11, 21, 01, 58, 14, 615, DateTimeKind.Utc);
 }
 
 
