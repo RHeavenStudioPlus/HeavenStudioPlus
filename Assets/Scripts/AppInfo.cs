6469e1baf18f92377ab4514a5d63fbde70ada7b7
RaffyTaffy14 <105959219+RaffyTaffy14@users.noreply.github.com>
2024-02-18 20:25:26 -0500

    Sumo brothers (#706)
    * Lockstep
    
    fansodnason
    
    * More Lockstep
    
    Implemented the new sprites (still needs work). Added new Icon.
    
    * More lockstep stuff
    
    i have no clue
    
    * Animation Tweaking
    
    I fiddled around with numbers weeeeeeeeeeeeeeee
    
    * Merge branch 'master' of https://github.com/RaffyTaffy14/HS
    
    * Background
    
    Has basic background
    
    * End Pose Start
    
    Adds the End Pose cue's tweeting sound (took so long but felt so cool)
    
    * Inu Sensei Start
    
    Inu Sensei added to scene. He can bop, crouch, or do neither
    
    * Temp Sumo Brothers Icon
    
    Made by Donfen
    
    * Inu Sensei Tweeting
    
    Inu Sensei now does his animation during the duration of the End Pose cue.
    
    * Crouch Block Rework
    
    Reworked crouch block to be a stretchable cue instead of a toggle (like KM prepare stance)
    
    * Bop Block Rework
    
    Bop block reworked to be a toggle. Also made a few minor tweaks to random things.
    
    * Added the Brothers
    
    The Sumo Brothers from Sumo Brothers are now in Sumo Brothers (minus the sumo)
    
    * Imported Upscaled Sumou_BG
    
    Higher resolution background and Inu Sensei sprites.
    
    * Imported REAL Upscaled Sumou_BG
    
    oops
    
    * Slap & Stomp Signals
    
    the domg buncus and tink tonks
    
    * Twiddling my Thumbs
    
    Random, Insignificant changes to random things in the code
    
    * idk
    
    random cleanup after I finally got HS to not be in shambles
    
    * more stuff
    
    dunno
    
    * Input for Pose
    
    this should not have taken this long
    
    * uh
    
    waste of 4gb atm, need 4gb atm, may need this not atm
    
    * Modernized Bops & WIP Stomps/Slaps
    
    Made the bop system to have automatic/stretchy settings like all new games. Also got a WIP implementation of stomping/slapping
    
    * quite honest i do not know
    
    let's just update HS
    
    * Prepare Slap Animation
    
    Prepare Slap animation has been animated and implemented
    
    * Sumo Prefab
    
    some stuff went down yesterday when updating the fork that involved the sumo prefab randomly downgrading to an older version, so I'm hoping that this commit will make sure that the current prefab wont just vanish into oblivion
    
    * Slapping Animation v1
    
    Spent a few hours animating the front-hand slapping animation. Also added a "Look at Camera" event that makes the brotheren look at the camera while slapping.
    
    * Stomping Animations
    
    All stomp and stomp prepare animations are animated and coded into the game (except for the miss one)
    
    * Slapping Animation v2
    
    finally animated the alternate slapping animation where those sumo brothers slap with their other hand
    
    * Slapping to Stomping Transition
    
    Added the slapping animation for when the Bros transition from slapping to stomping as well as all the accompanying code to get it to function. Also tweaked the two other slapping animations to fix the interpolation of those speed lines.
    
    * Inu Sensei Complete
    
    Added redone inu sensei spritesheet (thanks Mage!), animated all inu sensei animations, and coded all the inu sensei functionality. This is all really coming together now
    
    * Sumo Miss anims & more
    
    Added the Slap/Stomp miss animations. Coded the ability to whiff inputs. Replaced all the SFX with Kievit-approved rips. Added the little slap impact effect for successful slap inputs. Getting ready to make stomps pretty. Fixed misc bugs.
    
    * Sumo Pose anims & more
    
    Added the first set of pose animations. Also added animations for transitioning from posing to something else. Stomps aint getting pretty this time. Fixed misc bugs.
    
    * Sumo Pose anims v2
    
    Animated the whole 2nd set of sumo pose animations. Fixed some various bugs with poses. Added a random setting for the poses (although it doesn't work well with only 2 poses). Getting ready for the PR
    
    * Random Bug Fixes
    
    Fixed random pose option, fixed an animation that accidentally left loop on, and added monologue. Getting ready for pr in a few days.
    
    * New Icon & Bug Fixes
    
    Added a shiny new icon made by Seanski2, fixed a bug about poses in tap mode being a bit bugged and fixed the Slap-to-Stomp slap animation being able to be played too soon.
    
    ---------
    
    Co-authored-by: minenice55 <star.elementa@gmail.com>

--- Assets/Scripts/AppInfo.cs
+++ Assets/Scripts/AppInfo.cs
@@ -1,8 +1,8 @@
 using System;
 
 public static class AppInfo {
-    public const string Version = "0.0.1018";
-    public static readonly DateTime Date = new DateTime(2024, 01, 15, 19, 39, 36, 317, DateTimeKind.Utc);
+    public const string Version = "1.0.4";
+    public static readonly DateTime Date = new DateTime(2024, 02, 16, 02, 44, 34, 828, DateTimeKind.Utc);
 }
 
 
