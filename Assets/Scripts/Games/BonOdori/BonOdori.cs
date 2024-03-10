ef906e9f08b5ceca27d2afe7571eb7977b4cb683
streitixy <85808002+streitixy@users.noreply.github.com>
2024-03-09 21:36:44 -0300

    pa, pa-n and do, do-n were switched, this pr fixes it (#769)
    * started working on bon odori
    
    * bon odori is now playable, just no animations and sounds
    
    * bon odori is functional now, just missing the art, better audio and animations
    
    * nothing new, my git is always 1 commit behind
    
    * Revert "nothing new, my git is always 1 commit behind"
    
    This reverts commit b96a70004de5964902f7bc87d819a9e6047e77fb.
    
    * changed the background
    
    im only commiting because saladplainzone is gonna do the anims now
    
    * Accurate BG
    
    * Good prefab
    
    * finalized player prefab
    
    * Finalize Prefab
    
    * More animation stuff
    
    * Bow anim done
    
    * text is now functional, passing the project to AstrlJelly
    
    * merging w master branch
    
    * text scrolling is ALMOST functional
    
    * scrolling is ALMOST ALMOST functional
    
    * FINALLY!!!! TEXT SCOLLING IS DONE!!!!!
    
    * TEXT SCROLLING IS (almost) PERFECT!!!! now we gotta wait for the animations
    
    * minor bug fixes
    
    * TEXT SCROLLING IS ACTUALLY PERFECT NOW. also updated the font
    
    * i forgor to change the outline on the fonts
    
    * some bon odori changes (please dont break anything please)
    
    * LAST COMMIT BEFORE PUSHING!!!!!!!
    
    * forgot fix some minor things lol
    
    * organized the folders and made the dark bg an ease and not an animation
    
    * super cool commit
    
    * bug fixes
    
    * added pitching to pans and dons
    
    * changing Clap() and Sound() to a prefunction
    
    * prefunctions done :thumbsup:
    
    * the smallest change ever
    
    ---------
    
    Co-authored-by: saladplainzone <chocolate2890mail@gmail.com>
    Co-authored-by: wookywok <62037083+wookywok@users.noreply.github.com>
    Co-authored-by: minenice55 <star.elementa@gmail.com>

--- Assets/Scripts/Games/BonOdori/BonOdori.cs
+++ Assets/Scripts/Games/BonOdori/BonOdori.cs
@@ -445,8 +445,8 @@ namespace HeavenStudio.Games
                 string clip = typeSpeak switch
                 {
                     0 => "pan",
-                    1 => "pa_n",
-                    2 or _ => "pa",
+                    1 => "pa",
+                    2 or _ => "pa_n",
                 };
                 var pitch = SoundByte.GetPitchFromSemiTones(semitone, true);
                 SoundByte.PlayOneShotGame($"bonOdori/" + clip + (variation + 1), beat, pitch);
@@ -463,8 +463,8 @@ namespace HeavenStudio.Games
             string clip = typeSpeak switch
             {
                 0 => "don",
-                1 => "do_n",
-                2 or _ => "do",
+                1 => "do",
+                2 or _ => "do_n",
 
             };
             var pitch = SoundByte.GetPitchFromSemiTones(semitone, true);
