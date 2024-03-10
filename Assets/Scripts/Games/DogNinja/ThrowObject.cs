572b9e1f4d335ff009130db84cde6bf089f40337
AstrlJelly <bdlawson115@gmail.com>
2024-03-09 19:36:41 -0500

    Dog Ninja Rework (#767)
    * nearly the entire dog ninja rework
    
    lol. didn't think it would be this easy
    just a few more things to fix and we're good
    
    * fix some things, add NOT WORKING updater
    
    * final optimizations and fixes
    
    damn i didn't think the preparing stuff would be such a big deal
    
    * actual last fixes + ass buns

--- Assets/Scripts/Games/DogNinja/ThrowObject.cs
+++ Assets/Scripts/Games/DogNinja/ThrowObject.cs
@@ -10,6 +10,8 @@ namespace HeavenStudio.Games.Scripts_DogNinja
 {
     public class ThrowObject : MonoBehaviour
     {
+        [SerializeField] DogNinja game;
+
         public double startBeat;
         public int type;
         public bool fromLeft;
@@ -26,120 +28,84 @@ namespace HeavenStudio.Games.Scripts_DogNinja
 
         [Header("References")]
         public BezierCurve3D curve;
-        [SerializeField] BezierCurve3D barelyCurve;
+        [SerializeField] BezierCurve3D LeftCurve;
+        [SerializeField] BezierCurve3D RightCurve;
+        private BezierCurve3D barelyCurve;
         [SerializeField] BezierCurve3D BarelyLeftCurve;
         [SerializeField] BezierCurve3D BarelyRightCurve;
-        [SerializeField] GameObject HalvesLeftBase;
-        [SerializeField] GameObject HalvesRightBase;
-        [SerializeField] Transform ObjectParent;
+        [SerializeField] SpawnHalves HalvesLeftBase;
+        [SerializeField] SpawnHalves HalvesRightBase;
         public Sprite[] objectLeftHalves;
         public Sprite[] objectRightHalves;
 
-        private DogNinja game;
-        
-        private void Awake()
-        {
-            game = DogNinja.instance;
-            DogAnim = game.DogAnim;
-        }
-
         private void Start()
         {
+            DogAnim = game.DogAnim;
+            curve = fromLeft ? LeftCurve : RightCurve;
             barelyCurve = fromLeft ? BarelyRightCurve : BarelyLeftCurve;
-            
-            game.ScheduleInput(startBeat, 1f, DogNinja.InputAction_Press, Hit, Miss, Out);
+
+            game.ScheduleInput(startBeat, 1f, DogNinja.InputAction_Press, Hit, Miss, null);
         }
 
         private void Update()
         {
-            float flyPos = Conductor.instance.GetPositionFromBeat(startBeat, 1f)+1.1f;
-            float flyPosBarely = Conductor.instance.GetPositionFromBeat(barelyTime, 1f)+1f;
             if (isActive) {
+                float flyPos = game.conductor.GetPositionFromBeat(startBeat, 1f)+1.1f;
                 flyPos *= 0.31f;
                 transform.position = curve.GetPoint(flyPos);
                 objPos = curve.GetPoint(flyPos);
                 // destroy object when it's off-screen
                 if (flyPos > 1f) {
-                    GameObject.Destroy(gameObject);
+                    Destroy(gameObject);
                 }
             } else {
+                float flyPosBarely = game.conductor.GetPositionFromBeat(barelyTime, 1f)+1f;
                 flyPosBarely *= 0.3f;
                 transform.position = barelyCurve.GetPoint(flyPosBarely) + objPos;
                 float rot = fromLeft ? 200f : -200f;
                 transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + (rot * Time.deltaTime));
                 if (flyPosBarely > 1f) {
-                    GameObject.Destroy(gameObject);
+                    Destroy(gameObject);
                 }
             }
-
-            if ((!Conductor.instance.isPlaying && !Conductor.instance.isPaused) 
-                || GameManager.instance.currentGame != "dogNinja") {
-                GameObject.Destroy(gameObject);
-            }
-        }
-
-        private void SuccessSlice() 
-        {
-            string slice = "Slice" + direction switch
-            {
-                0 => "Left",
-                1 => "Right",
-                _ => "Both",
-            };
-
-            DogAnim.DoScaledAnimationAsync(slice, 0.5f);
-            if (shouldSfx) SoundByte.PlayOneShotGame(sfxNum+"2");
-
-            game.WhichLeftHalf.sprite = objectLeftHalves[type-1];
-            game.WhichRightHalf.sprite = objectRightHalves[type-1];
-
-            SpawnHalves LeftHalf = Instantiate(HalvesLeftBase, game.gameObject.transform).GetComponent<SpawnHalves>();
-            LeftHalf.startBeat = startBeat;
-            LeftHalf.lefty = fromLeft;
-            LeftHalf.objPos = objPos;
-
-            SpawnHalves RightHalf = Instantiate(HalvesRightBase, game.gameObject.transform).GetComponent<SpawnHalves>();
-            RightHalf.startBeat = startBeat;
-            RightHalf.lefty = fromLeft;
-            RightHalf.objPos = objPos;
-
-            GameObject.Destroy(gameObject);
-        }
-
-        private void JustSlice()
-        {
-            isActive = false;
-            barelyTime = Conductor.instance.songPositionInBeatsAsDouble;
-
-            string barely = "Barely" + direction switch
-            {
-                0 => "Left",
-                1 => "Right",
-                2 => "Both",
-                _ => "Both",
-            };
-
-            DogAnim.DoScaledAnimationAsync(barely, 0.5f);
-            SoundByte.PlayOneShotGame("dogNinja/barely");
         }
 
         private void Hit(PlayerActionEvent caller, float state)
         {
-            game.DogAnim.SetBool("needPrepare", false);
-            if (state >= 1f || state <= -1f) JustSlice();
-                else SuccessSlice();
+            game.StopPrepare();
+            string dir = direction switch {
+                0 => "Left",
+                1 => "Right",
+                _ => "Both",
+            };
+            if (state >= 1f || state <= -1f) {
+                isActive = false;
+                barelyTime = game.conductor.songPositionInBeatsAsDouble;
+
+                DogAnim.DoScaledAnimationAsync("Barely" + dir, 0.5f);
+                if (shouldSfx) SoundByte.PlayOneShotGame("dogNinja/barely");
+            } else {
+                DogAnim.DoScaledAnimationAsync("Slice" + dir, 0.5f);
+                if (shouldSfx) SoundByte.PlayOneShotGame(sfxNum + "2");
+
+                HalvesLeftBase.sr.sprite = objectLeftHalves[type - 1];
+                HalvesRightBase.sr.sprite = objectRightHalves[type - 1];
+                for (int i = 0; i < 2; i++) {
+                    SpawnHalves half = Instantiate(i == 0 ? HalvesLeftBase : HalvesRightBase, game.transform);
+                    half.startBeat = startBeat;
+                    half.lefty = fromLeft;
+                    half.objPos = objPos;
+                }
+
+                Destroy(gameObject);
+            }
         }
 
         private void Miss(PlayerActionEvent caller)
         {
-            if (!DogAnim.GetBool("needPrepare")) return;
-            DogAnim.DoScaledAnimationAsync("UnPrepare", 0.5f);
-            DogAnim.SetBool("needPrepare", false);
-        }
-
-        private void Out(PlayerActionEvent caller) 
-        {
-            DogAnim.SetBool("needPrepare", false);
+            if (!game.preparing) return;
+            DogAnim.DoScaledAnimationAsync("Unprepare", 0.5f);
+            game.StopPrepare();
         }
     }
 }
